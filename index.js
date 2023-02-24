efc459f18cdcf429ad0266e8784e60c5c76faa04
Essedger <essedger@gmail.com>
2023-02-24 23:08:08 +0400

    Add env, connect DB with creds

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const authRouter = require('./src/authRouter')
-const PORT = process.env.PORT || 5000
+require('dotenv').config();
+const PORT = process.env.PORT || 5005
 const USERNAME = process.env.DB_USERNAME
 const DB_PASSWORD = process.env.DB_PASSWORD
 
@@ -12,7 +13,8 @@ app.use("/auth", authRouter)
 
 const start = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://${USERNAME}:${DB_PASSWORD}@cluster0.b6pb9.mongodb.net/auth_roles?retryWrites=true&w=majority`)
+        // await mongoose.connect(`mongodb+srv://${USERNAME}:${DB_PASSWORD}@library.yz89zfk.mongodb.net/?retryWrites=true&w=majority`)
+        await mongoose.connect(`mongodb+srv://${USERNAME}:${DB_PASSWORD}@library.yz89zfk.mongodb.net/?retryWrites=true&w=majority`)
         app.listen(PORT, () => console.log(`server started on port ${PORT}`))
     } catch (e) {
         console.log(e)
